75010eb44845b07d3f5ebd6ba96a9cf9d299b27c
Matti <mattibylin@Mattis-Air.ZyXEL-USG>
2020-03-10 07:10:00 -0500

    Creamos la estructura con os diferentes componentes, servicios y pipes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './home/nav/nav.component';
+import { ProductComponent } from './home/product/product.component';
+import { CatalogComponent } from './home/catalog/catalog.component';
+import { CartComponent } from './home/cart/cart.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavComponent,
+    ProductComponent,
+    CatalogComponent,
+    CartComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,

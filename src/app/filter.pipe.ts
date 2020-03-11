06d40211962f98dae4e479789ed79e2b0ed04205
Matti <mattibylin@Mattis-Air.ZyXEL-USG>
2020-03-11 08:27:20 -0500

    Se desarrolla el Pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(items: any, field:string, searchText: string): any {
+    if (!items) return [];
+    if(!searchText || searchText==="") return items;
+    searchText = searchText;
+    return items.filter(it => {
+      return it['Nombre'].includes(searchText);
+    });
   }
 
 }

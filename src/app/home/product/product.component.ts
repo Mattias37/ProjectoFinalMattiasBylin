5acf15e27b9d69dc9173d249db24814aff16709d
Matti <mattibylin@Mattis-Air.ZyXEL-USG>
2020-03-11 12:11:27 -0500

    Revision visión detallada de productos

--- src/app/home/product/product.component.ts
+++ src/app/home/product/product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../../data.service";
+import { Observable } from 'rxjs';
+import 'firebase/firestore';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-product',
@@ -6,8 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  product:any = undefined;
+  index:any = undefined;
+  constructor(private dataService: DataService,
+              private route: ActivatedRoute) {
+    route.params.subscribe( parametros => {
+      dataService.items( parametros['index'])
+      .subscribe( res => {
+        this.product = res.json();
+      })
+    })
+   }
 
   ngOnInit() {
   }
